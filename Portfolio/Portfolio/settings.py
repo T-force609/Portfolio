8598fd834ad0faadc0a2fe7acce40b418668f50c
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-06-25 11:30:43 +0100

    Update settings.py

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -89,6 +89,8 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 CORS_ALLOWED_ORIGINS =[
     'https://codewithajdev.com',
     "http://www.codewithajdev.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
