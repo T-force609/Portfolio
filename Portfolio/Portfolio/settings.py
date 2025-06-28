08f2f8f57cc9546b1f5afb2256d2443ca5d3cb83
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-06-28 10:01:25 +0100

    Update settings.py

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -154,8 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = [BASE_DIR / 'custom_admin/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
@@ -167,7 +167,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-EMAIL_BACKEND = 'django.core.mail.backend.smtp.EmailBack'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'sntp.ajibadejohn30@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
