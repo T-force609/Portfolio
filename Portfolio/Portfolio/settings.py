e5eae7b489ba0fe86d3e3431a6ff96ff44016c4a
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-07-04 11:00:28 +0100

    Update settings.py

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -113,7 +113,8 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600}
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+}
 
 
 # Password validation
