730b86c5a5f5ce6d6a6620462c48b9b92e1bf6f5
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-07-15 13:05:57 +0100

    Update settings.py

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.ajibadejohn30@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ajibadejohn30@gmail.com'
