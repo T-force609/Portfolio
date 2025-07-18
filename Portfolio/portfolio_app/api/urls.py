98fe9444173a86f100345e9652ec17063ca9af8a
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-07-18 14:55:18 +0100

    Update urls.py

--- Portfolio/portfolio_app/api/urls.py
+++ Portfolio/portfolio_app/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .. import views
+from rest_framework.router import DefaultRouter
 from ..views import (SkillListView, 
                      SkillDetialView, 
                      ProjectDetailView, 
@@ -7,11 +8,18 @@ from ..views import (SkillListView,
                      AdminPostUpload,
                      ContactRequestViewSet)
 
+
+router = DefaultRouter()
+router.register('r contact_request', ContactRequestViewSet, name='contact_request')
+
+
+
+
 urlpatterns = [
     path('skills/', SkillListView.as_view(), name='skills_list'),
     path('projects/', ProjectListView.as_view(), name='project_list'),
     path('skills/<int:id>/', SkillDetialView.as_view(), name='skills_detail'),
     path('projects/<int:id>/', ProjectDetailView.as_view(), name='project_detail'),
     path('imagefiles/', AdminPostUpload.as_view(), name='imagefiles'),
-    path('contactrequests/', ContactRequestViewSet.as_view({'post': 'create'})),
+    path('api/', include(router.urls)),
     ]
