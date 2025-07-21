28826caf68fe8cf84c998bb3c28c7c93ee80826e
T-force609 <101106126+T-force609@users.noreply.github.com>
2025-07-21 15:11:22 +0100

    Update urls.py

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -1,15 +1,18 @@
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from portfolio_app.views import ContactRequestViewSet
+
+router = DefaultRouter()
+router.register(r'contact_request', ContactRequestViewSet, basename='contact_request')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('custom_admin/', include('custom_admin.urls')),
     path('api/', include('portfolio_app.api.urls')),
     path('api-auth/', include('rest_framework.urls'))
+    path('api/', include(router.urls)),
 ]
 
 if settings.DEBUG:
